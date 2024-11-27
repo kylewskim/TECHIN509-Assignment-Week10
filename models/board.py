073b071c77d46c0b40a5a61fd613ca7cfd2a5daf
Kyle <kylewskim@gmail.com>
2024-11-26 17:03:37 -0800

    update board.py

--- models/board.py
+++ models/board.py
@@ -6,6 +6,16 @@ class Board:
         """
         Draw the board of Tic-Tac-Toe game
         """
+        for row in range(3): # based on the number of row, create 2 lines repetitively
+            print(" ---" * 3)
+            print("|", end="") # start with one |, not changing line
+            for col in range(3): # based on the number of col, fill in the matrix
+                if (self.grid[row][col] == ""):
+                    print("   |", end="")
+                else:
+                    print(f" {self.grid[row][col]} |", end="")
+                print() # change line
+        print(" ---" * 3) # finalize with the last line
 
     def update_board(self, row: int, col: int, symbol: str) -> bool:
         """
@@ -28,6 +38,29 @@ class Board:
         Returns:
             str: The winning symbol ('X' or 'O') if there is a winner, else an empty string
         """
+        draw = 1
+
+        for row in range(3):
+            if row[0] == row[1] == row[2] and row[0] != "": # to prevent the whole row is empty
+                draw = 0
+                return row[0]
+            
+        for col in range(3):
+            if self.grid[0][col] == self.grid[1][col] == self.grid[2][col] and self.grid[0][col] != "": # to prevent the whole col is empty
+                draw = 0
+                return self.grid[0][col]
+        
+        if self.grid[0][0] == self.grid[1][1] == self.grid[2][2] and self.grid[0][0] != "": # to prevent the whole diagonal line is empty
+            draw = 0
+            return self.grid[0][0]
+        
+        if self.grid[0][2] == self.grid[1][1] == self.grid[2][0] and self.grid[0][2] != "": # to prevent the whole diagonal line is empty
+            draw = 0
+            return self.grid[0][2]
+        
+        if draw == 0:
+            draw = 1
+            return ""
 
     def is_full(self) -> bool:
         """
